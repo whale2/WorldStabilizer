305e3073f593a5c6c759cb10badc44d7a9421289
Nikita Makeev <whale2.box@gmail.com>
2017-12-11 00:58:40 +0100

    Tried to work around mysteriois situation where raycast from bottom point does not hit the ground, but hits something beneath. Excluded KerbalEVA from stabilizing. Tweaked some safety params - 110% up, 90% down.

--- WorldStabilizer/WorldStabilizer.cs
+++ WorldStabilizer/WorldStabilizer.cs
@@ -32,8 +32,7 @@ namespace WorldStabilizer
 			bounds = new Dictionary<Guid, VesselBounds> ();
 			anchors = new Dictionary<Guid, List<PartModule>> ();
 			GameEvents.onVesselGoOffRails.Add (onVesselGoOffRails);
-			GameEvents.onVesselCreate.Add(onVesselCreate);
-			GameEvents.onNewVesselCreated.Add (onNewVesselCreated);
+			//GameEvents.onVesselCreate.Add(onVesselCreate);
 			stabilizationTimer = (int)(stabilizationFactor / Time.fixedDeltaTime);  // If delta = 0.01, we will keep vessels 300 frames
 		}
 
@@ -41,20 +40,18 @@ namespace WorldStabilizer
 			printDebug("OnDestroy");
 			GameEvents.onVesselGoOffRails.Remove (onVesselGoOffRails);
 			GameEvents.onVesselCreate.Remove(onVesselCreate);
-			GameEvents.onNewVesselCreated.Remove (onNewVesselCreated);
 		}
 
 		public void onVesselCreate(Vessel v) {
-			printDebug ("vessel = " + v.name);
-		}
-
-		public void onNewVesselCreated(Vessel v) {
 			printDebug ("vessel = " + v.name);
 		}
 			
 		public void onVesselGoOffRails(Vessel v) {
 
 			if (v.situation != Vessel.Situations.LANDED && v.situation != Vessel.Situations.PRELAUNCH)
+				return;
+
+			if (v.isEVA) // Kerbals are usually ok
 				return;
 
 			printDebug("off rails: " + v.name + "; fixedDelta = " + Time.fixedDeltaTime + "; delta = " + Time.deltaTime);
@@ -80,35 +77,39 @@ namespace WorldStabilizer
 		}
 
 		private void moveUp(Vessel v) {
-			float alt = GetRaycastAltitude (v, bounds[v.id].bottomPoint,  (1<<15)|(1<<0)); //mask: ground and parts
+			float alt = GetRaycastAltitude (v, bounds[v.id].bottomPoint,  (1<<28)|(1<<15)|(1<<0)); //mask: ground and parts
 			float alt2 = GetRaycastAltitude(v, bounds[v.id].topPoint, 1<<15); // mask: ground only
-			if (alt <= 0) {
+			//if (alt <= 0) {
 
 				float upMovement = bounds [v.id].bottomLength + bounds [v.id].topLength - alt2;
 				if (upMovement > 0) {
 					printDebug ("Moving up: " + v.name + " by " + upMovement + "; alt = " + alt.ToString () + "; alt from top = " + alt2.ToString ());
 					Vector3 up = (v.transform.position - FlightGlobals.currentMainBody.transform.position).normalized;
-					v.Translate (up * upMovement);
+					v.Translate (up * upMovement * 1.1f);
 
 					printDebug ("Moved: " + v.name + "; alt = " + GetRaycastAltitude (v, bounds [v.id].bottomPoint, (1 << 15) | (1 << 0)).ToString () +
 					"; alt from top = " + GetRaycastAltitude (v, bounds [v.id].topPoint, 1 << 15).ToString ());
 				} else {
 					printDebug ("upMovement is " + upMovement + "; not moving up");
 				}
-			} else {
-				printDebug ("altitude is " + alt + "; not moving up because above the ground");
-			}
+			//} else {
+			//	printDebug ("altitude is " + alt + "; alt2 = " + alt2 + "; not moving up because above the ground");
+			//}
 		}
 
 		private void moveDown(Vessel v) {
 
 			bounds [v.id].findBoundPoints ();
-			float alt = GetRaycastAltitude (v, bounds[v.id].bottomPoint,  (1<<15)|(1<<0));
+			float alt = GetRaycastAltitude (v, bounds[v.id].bottomPoint,  (1<<28)|(1<<15)|(1<<0));
 			float alt3 = GetRaycastAltitude(v, bounds[v.id].topPoint, 1<<15);
+			// Somtimes we hit some strange things with raycast, or don't hit actual terrain and height from 
+			// bottom point is greater than radar altitude, i.e. height from CoM. In this case, use raycast from topmost
+			// point.
+			float downMovement = alt < v.radarAltitude ? alt : bounds [v.id].bottomLength + bounds [v.id].topLength - alt3;
 			printDebug ("Moving down: " + v.name + "; alt = " + alt.ToString() + "; timer = " + vessel_timer[v.id] + "; radar alt = " + v.radarAltitude +
 				"; alt from top = " + alt3.ToString());
 			Vector3 up = (v.transform.position - FlightGlobals.currentMainBody.transform.position).normalized;
-			v.Translate (alt * -up * 0.97f);
+			v.Translate (downMovement * -up * 0.9f);
 		}
 
 		private void stabilize(Vessel v) {
